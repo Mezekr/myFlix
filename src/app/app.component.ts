04b963e610482654d61a8d201c65a407e07210fa
Mezekr W <mezi.edu@tuta.io>
2024-04-30 21:33:55 +0200

    move open dialog  to Welcome Page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { MovieCardComponent } from './movie-card/movie-card.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @Component({
   selector: 'app-root',
@@ -11,28 +8,4 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) {}
-
-  // opens the dialog when the Signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '320px',
-    });
-  }
-
-  // opens the dialog when the login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '320px',
-    });
-  }
-
-  // opens the Movies dialog when the All movies button is clicked
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '320px',
-    });
-  }
 }
