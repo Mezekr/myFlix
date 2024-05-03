9540b4aaeabc9bd06aebf80a28b2c97c0ad0c246
Mezekr W <mezi.edu@tuta.io>
2024-05-03 19:10:02 +0200

    refactro movie card components

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,52 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { FetchApiDataService } from '../fetch-api-data.service';
 
-// Comppnents
+import { FetchApiDataService } from '../fetch-api-data.service';
+// Components
 import { DirectorInfoComponent } from '../director-info/director-info.component';
+import { GenreInfoComponent } from '../genre-info/genre-info.component';
 import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.scss',
+  styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
+  user: any = {};
+  userData = { Username: '', FavoriteMovies: [] };
+  FavoriteMovies: any[] = [];
+  isFavMovie: boolean = false;
+
   constructor(
     public fetchApiData: FetchApiDataService,
-    public snackBar: MatSnackBar,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
     this.getMovies();
+    this.getFavMovies();
   }
 
+  // getting all movies.
   getMovies(): void {
-    this.fetchApiData.getAllMovies().subscribe({
-      next: (response: any) => {
-        this.movies = response;
-        console.log(this.movies);
-        return this.movies;
-      },
-      error: (error) => {
-        this.snackBar.open(error.message, 'OK', {
-          duration: 2000,
-          verticalPosition: 'top',
-        });
-      },
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
     });
   }
 
-  openSynopsisDialog(movie: any): void {
-    this.dialog.open(MovieSynopsisComponent, {
-      data: { movie },
-      maxWidth: '460px',
+  // Gets favorites Movie list
+  getFavMovies(): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.FavoriteMovies = this.user.FavouriteMovies;
+    this.FavoriteMovies = this.user.FavouriteMovies;
+    console.log('Fav Movies in getFavMovie', this.FavoriteMovies);
+  }
+
+  // check if movie is a favorite movie.
+  isFav(movie: any): any {
+    const MovieID = movie._id;
+    if (this.FavoriteMovies?.some((movie) => movie === MovieID)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // toggles favorite movies or not
+  toggleFav(movie: any): void {
+    const isFavorite = this.isFav(movie);
+    isFavorite ? this.deleteFavMovies(movie) : this.addFavMovies(movie);
+  }
+
+  // Adds movie from favMovie list
+  addFavMovies(movie: any): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.Username = this.user.Username;
+    this.fetchApiData.addFavouriteMovies(movie).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result));
+      this.getFavMovies();
+      this.snackBar.open('Movie has been added to your favorites!', 'OK', {
+        duration: 3000,
+        verticalPosition: 'top',
+      });
     });
   }
 
+  // Deletes movie from favMovie list.
+  deleteFavMovies(movie: any): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.Username = this.user.Username;
+    this.fetchApiData.deleteFavouriteMovies(movie).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result));
+      this.getFavMovies();
+      this.snackBar.open('Movie has been deleted from your favorites!', 'OK', {
+        duration: 3000,
+        verticalPosition: 'top',
+      });
+    });
+  }
+
+  // opens the dialog when director button is clicked
   openDirectorDialog(name: string, bio: string): void {
     this.dialog.open(DirectorInfoComponent, {
       data: {
@@ -54,6 +100,25 @@ export class MovieCardComponent {
         Bio: bio,
       },
       width: '500px',
+    });
+  }
+
+  // opens the dialog when genre button is clicked.
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreInfoComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: '500px',
+    });
+  }
+
+  // opens the dialog when synopsis button is clicked
+  openSynopsisDialog(movie: any): void {
+    this.dialog.open(MovieSynopsisComponent, {
+      data: { movie },
+      maxWidth: '460px',
     });
   }
 }
