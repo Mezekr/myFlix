270500ce971a2cf1e2f9e00c60e70cf9a752a4ca
Mezekr W <mezi.edu@tuta.io>
2024-05-03 19:08:41 +0200

    add date picker meterial module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import {
   BrowserModule,
   provideClientHydration,
@@ -17,19 +20,22 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectorInfoComponent } from './director-info/director-info.component';
+import { GenreInfoComponent } from './genre-info/genre-info.component';
+import { HeaderComponent } from './header/header.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { MovieSynopsisComponent } from './movie-synopsis/movie-synopsis.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { HeaderComponent } from './header/header.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { MovieSynopsisComponent } from './movie-synopsis/movie-synopsis.component';
-import { DirectorInfoComponent } from './director-info/director-info.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: UserProfileComponent },
 ];
 
 @NgModule({
@@ -43,6 +49,8 @@ const routes: Routes = [
     UserProfileComponent,
     MovieSynopsisComponent,
     DirectorInfoComponent,
+    GenreInfoComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -58,8 +66,14 @@ const routes: Routes = [
     MatSnackBarModule,
     RouterModule.forRoot(routes),
     MatIconModule,
+    MatToolbarModule,
+    MatDatepickerModule,
   ],
-  providers: [provideClientHydration(), provideAnimationsAsync()],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideNativeDateAdapter(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
